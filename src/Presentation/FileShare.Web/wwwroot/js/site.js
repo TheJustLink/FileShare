b2d42bd67bf13c36b41c5fe6f10399f7cc69fe50
Link <devlinkgames@gmail.com>
2024-01-31 19:24:09 +0700

    Add dropzone plugin for upload frontend

--- src/Presentation/FileShare.Web/wwwroot/js/site.js
+++ src/Presentation/FileShare.Web/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+Dropzone.options.FileUpload = {
+    paramName: "File",
+    maxFilesize: 25, // MB
+
+    success: function (file) {
+        window.location.href = file.xhr.responseURL;
+    }
+};
