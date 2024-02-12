10ff16720492bc3ec69216d677eb3d5c31834ab5
Link <devlinkgames@gmail.com>
2024-02-12 23:24:20 +0700

    Add docker volume for .aspnet, fix/improve styles, animations, frontend improvements

--- src/Presentation/FileShare.Web/wwwroot/js/site.js
+++ src/Presentation/FileShare.Web/wwwroot/js/site.js
@@ -7,8 +7,45 @@ Dropzone.options.FileUpload = {
     paramName: "File",
     maxFilesize: 25, // MB
     dictDefaultMessage: "Drop file here",
+    createImageThumbnails: false,
+    maxFiles: 1,
 
     success: function (file) {
-        //window.location.href = file.xhr.responseURL;
+        window.location.href = file.xhr.responseURL;
     }
 };
+
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+    }
+
+    document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    }
+    navigator.clipboard.writeText(text).then(function () {
+        console.log('Async: Copying to clipboard was successful!');
+    }, function (err) {
+        console.error('Async: Could not copy text: ', err);
+    });
+}
